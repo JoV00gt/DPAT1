fabbda1176aec33070c25aaeb6f0fbd883e44cd3
Wesley Vink <w.vink2@student.avans.nl>
2025-06-22 19:28:48 +0200

    split program

--- DPAT1/DPAT1/Program.cs
+++ DPAT1/DPAT1/Program.cs
@@ -9,20 +9,31 @@ class Program
 {
     static void Main()
     {
-        string filePath = @"../../../FSMFiles/invalid_deterministic1.fsm";
+        string filePath = @"../../../FSMFiles/example_user_account.fsm";
 
+        FSM? fsm = Build(filePath);
+        if (fsm != null)
+        {
+            Validation(fsm);
+            Render(fsm);
+        }
+    }
+    private static FSM? Build(string filePath)
+    {
         if (!File.Exists(filePath))
         {
             Console.WriteLine("File not found.");
-            return;
+            return null;
         }
 
         FSMBuilder builder = new FSMBuilder();
         Director director = new Director(builder);
         director.CreateFSM(filePath, builder.GetFSM());
-        var fsm = builder.GetFSM();
+        return builder.GetFSM();
+    }
+    private static void Validation(FSM fsm)
+    {
         var context = new FSMValidationContext();
-
         bool isNonDetValid = ValidateWith(context, fsm, new NonDeterministicTransitionsValidator(), "Non-deterministic transitions");
         bool isInitialValid = ValidateWith(context, fsm, new InitialStateTransitionsValidator(), "Initial state with incoming transitions");
         bool isReachableValid = ValidateWith(context, fsm, new UnreachableStateValidator(), "Unreachable states");
@@ -30,13 +41,15 @@ class Program
         bool allValid = isNonDetValid && isInitialValid && isReachableValid;
         Console.WriteLine();
         Console.WriteLine(allValid ? "FSM is Valid!" : "FSM contains validation errors");
+    }
+
+    private static void Render(FSM fsm)
+    {
         IUIFactory factory = new ConsoleIOFactory(fsm);
         IOutputRenderer renderer = factory.CreateRenderer();
         IVisitor visitor = factory.CreateVisitor();
         renderer.Accept(visitor);
-
     }
-
     private static bool ValidateWith(FSMValidationContext context, FSM fsm, IFSMValidator strategy, string description)
     {
         context.SetStrategy(strategy);
